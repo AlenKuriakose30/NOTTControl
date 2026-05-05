a8284f7573ff5182ac71b8e535c9f98c59733e56
KwintenMissiaen <kwinten.missiaen@kuleuven.be>
2026-05-05 16:19:42 +0200

    Show proper status for shutter; workaround as PLC timestamp is unavailable

--- nottcontrol/shutterwidget.py
+++ nottcontrol/shutterwidget.py
@@ -35,7 +35,7 @@ class ShutterWidget(QWidget):
             self.ui.label_status.setText(str(status))
             self.ui.label_state.setText(str(state))
             self.ui.label_subState.setText(str(substate))
-            hwStatus = self._shutter.getHardwareStatus()[0]
+            hwStatus = self.get_shutter_status
             self.ui.label_opened.setText(str(hwStatus))
         except Exception as e:
             print(e)
@@ -43,18 +43,26 @@ class ShutterWidget(QWidget):
     
     def load_position(self):
         try:
-            hwStatus, timestamp = self._shutter.getHardwareStatus()
-            timestamp_plc = datetime.strptime(timestamp, '%Y-%m-%d-%H:%M:%S.%f')
+            hwStatus, timestamp = self.get_shutter_status(), datetime.utcnow()
+            #timestamp_plc = datetime.strptime(timestamp, '%Y-%m-%d-%H:%M:%S.%f')
 
             shutter_pos = -1
-            if hwStatus == "OPEN":
+            if hwStatus == "Open":
                 shutter_pos = 1
-            if hwStatus == "CLOSED":
+            if hwStatus == "Closed":
                 shutter_pos = 0
-            self.redis_client.add_shutter_position(self._shutter.name, timestamp_plc, shutter_pos)
+            self.redis_client.add_shutter_position(self._shutter.name, timestamp, shutter_pos)
         except Exception as e:
             print(e)
             self.ui.label_error.setText(str(e))
+    
+    def get_shutter_status(self):
+        if self._shutter.is_open:
+            return "Open"
+        elif self._shutter.is_closed:
+            return "Closed"
+        else:
+            return "Moving"
 
     def reset(self):
         try:
